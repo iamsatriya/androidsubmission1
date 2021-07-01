84af01cf5fd5c91060e300393247ac92a470448b
Satriya <satriya.wicaksana888@gmail.com>
2021-07-01 11:32:30 +0700

    implementing bottom navigation

--- app/src/main/java/com/satriyawicaksana888/androidsubmission1/MainActivity.kt
+++ app/src/main/java/com/satriyawicaksana888/androidsubmission1/MainActivity.kt
@@ -3,7 +3,13 @@ package com.satriyawicaksana888.androidsubmission1
 import android.content.res.TypedArray
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.satriyawicaksana888.androidsubmission1.adapter.CardViewAdapter
 import com.satriyawicaksana888.androidsubmission1.data.User
 import com.satriyawicaksana888.androidsubmission1.databinding.ActivityMainBinding
@@ -14,11 +20,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.rvGithubUser.setHasFixedSize(true)
-        list.addAll(getListUser())
-        showRecyclerCardView()
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.navigation_home, R.id.navigation_search, R.id.navigation_profile
+        ).build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+//        binding.rvGithubUser.setHasFixedSize(true)
+//        list.addAll(getListUser())
+//        showRecyclerCardView()
     }
 
     private fun getListUser(): ArrayList<User> {
@@ -40,16 +55,16 @@ class MainActivity : AppCompatActivity() {
                 dataCompany[index],
                 dataFollowers[index],
                 dataFollowing[index],
-                dataAvatars.getResourceId(index,0),
+                dataAvatars.getResourceId(index, 0),
             )
             listUser.add(user)
         }
         dataAvatars.recycle()
         return listUser
     }
-    private fun showRecyclerCardView() {
-        binding.rvGithubUser.layoutManager = LinearLayoutManager(this)
-        val cardViewUserAdapter = CardViewAdapter(list, this)
-        binding.rvGithubUser.adapter = cardViewUserAdapter
-    }
+//    private fun showRecyclerCardView() {
+//        binding.rvGithubUser.layoutManager = LinearLayoutManager(this)
+//        val cardViewUserAdapter = CardViewAdapter(list, this)
+//        binding.rvGithubUser.adapter = cardViewUserAdapter
+//    }
 }
