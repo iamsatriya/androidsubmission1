d14ff93460382db60949ccdc1e428c94ff97b6f0
Satriya <satriya.wicaksana888@gmail.com>
2021-07-01 11:32:10 +0700

    adding fragment for detail user

---
+++ app/src/main/java/com/satriyawicaksana888/androidsubmission1/ui/detailuser/DetailUserFragment.kt
@@ -0,0 +1,18 @@
+package com.satriyawicaksana888.androidsubmission1.ui.detailuser
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.satriyawicaksana888.androidsubmission1.R
+
+class DetailUserFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_user, container, false)
+    }
+}
