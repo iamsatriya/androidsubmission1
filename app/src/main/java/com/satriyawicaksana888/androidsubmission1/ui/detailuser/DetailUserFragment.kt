7fccaec0400e42910d683125fcd37012f5af6afa
Satriya <satriya.wicaksana888@gmail.com>
2021-07-03 21:19:32 +0700

    implementing detail user fragment

--- app/src/main/java/com/satriyawicaksana888/androidsubmission1/ui/detailuser/DetailUserFragment.kt
+++ app/src/main/java/com/satriyawicaksana888/androidsubmission1/ui/detailuser/DetailUserFragment.kt
@@ -1,18 +1,40 @@
 package com.satriyawicaksana888.androidsubmission1.ui.detailuser
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.satriyawicaksana888.androidsubmission1.R
+import com.satriyawicaksana888.androidsubmission1.data.User
+import com.satriyawicaksana888.androidsubmission1.databinding.FragmentDetailUserBinding
 
 class DetailUserFragment : Fragment() {
+    private var _binding: FragmentDetailUserBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        const val EXTRA_USER = "extra_user"
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_user, container, false)
+        _binding = FragmentDetailUserBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val username = arguments?.getString(EXTRA_USER)
+        Log.e("user", "onViewCreated: $username", )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
