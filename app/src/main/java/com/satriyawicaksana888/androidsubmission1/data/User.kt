03e719e6742c5f8347eb2a9e5115cc6b9de57a55
iamsatriya <70894137+iamsatriya@users.noreply.github.com>
2021-07-05 14:42:21 +0700

    adding nullable to data class

--- app/src/main/java/com/satriyawicaksana888/androidsubmission1/data/User.kt
+++ app/src/main/java/com/satriyawicaksana888/androidsubmission1/data/User.kt
@@ -5,12 +5,13 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class User(
-    val username: String,
-    val name: String,
-    val location: String,
-    val repository: String,
-    val company: String,
-    val followers: String,
-    val following: String,
-    val avatars: Int,
+    val username: String?,
+    val name: String?,
+    val location: String?,
+    val repository: String?,
+    val company: String?,
+    val followers: String?,
+    val following: String?,
+    val avatarUrl: String?,
+    val htmlUrl: String?,
 ) : Parcelable
