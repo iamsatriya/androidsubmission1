33cd5521b3c8e9a276d7c79d5aca34426ff333ac
iamsatriya <70894137+iamsatriya@users.noreply.github.com>
2021-07-05 14:48:01 +0700

    move implementation to viewmodel

--- app/src/main/java/com/satriyawicaksana888/androidsubmission1/ui/search/SearchFragment.kt
+++ app/src/main/java/com/satriyawicaksana888/androidsubmission1/ui/search/SearchFragment.kt
@@ -122,55 +122,7 @@ class SearchFragment : Fragment() {
         binding.recycleView.adapter = cardViewUserAdapter
         cardViewUserAdapter.setOnItemClickCallback(object : CardViewAdapter.OnItemClickCallback {
             override fun onItemClicked(data: SearchUser) {
-                openDetailUser(data.username, context)
-            }
-        })
-    }
-
-    private fun openDetailUser(username: String?, context: Context) {
-        val client = GithubAccess.createClient()
-        client.get(username?.let { GithubAccess.urlDetailUser(it) }, object : AsyncHttpResponseHandler() {
-            override fun onSuccess(
-                statusCode: Int,
-                headers: Array<out Header>?,
-                responseBody: ByteArray?
-            ) {
-                try {
-                    val result = JSONObject(responseBody?.let { String(it) })
-                    val name = result.getString("name")
-                    val company = result.getString("company")
-                    val location = result.getString("location")
-                    val followers = result.getInt("followers").toString()
-                    val following = result.getInt("following").toString()
-                    val repository = result.getInt("public_repos").toString()
-                    val avatarUrl = result.getString("avatar_url")
-                    val htmlUrl = result.getString("html_url")
-                    val githubUser = User(
-                        username,
-                        name,
-                        location,
-                        repository,
-                        company,
-                        followers,
-                        following,
-                        avatarUrl,
-                        htmlUrl
-                    )
-                    val mIntent = Intent(context, ProfileActivity::class.java)
-                    mIntent.putExtra(ProfileActivity.EXTRA_USER, githubUser)
-                    context.startActivity(mIntent)
-                } catch (e: Exception) {
-                    Log.d(TAG, "onFailedToFetch: ${e.message}")
-                }
-            }
-
-            override fun onFailure(
-                statusCode: Int,
-                headers: Array<out Header>?,
-                responseBody: ByteArray?,
-                error: Throwable?
-            ) {
-                Log.d(TAG, "onFailure: ${error?.message}")
+                searchViewModel.openDetailUser(data.username, context)
             }
         })
     }
--- app/src/main/java/com/satriyawicaksana888/androidsubmission1/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/satriyawicaksana888/androidsubmission1/ui/search/SearchViewModel.kt
@@ -1,15 +1,23 @@
+import android.content.Context
+import android.content.Intent
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
+import com.satriyawicaksana888.androidsubmission1.ProfileActivity
 import com.satriyawicaksana888.androidsubmission1.data.SearchUser
+import com.satriyawicaksana888.androidsubmission1.data.User
+import com.satriyawicaksana888.androidsubmission1.ui.search.SearchFragment
 import com.satriyawicaksana888.androidsubmission1.utility.GithubAccess
 import cz.msebera.android.httpclient.Header
 import org.json.JSONObject
 
 class SearchViewModel : ViewModel() {
+    companion object {
+        val TAG = SearchViewModel::class.java.simpleName
+    }
     val listUsersLiveData = MutableLiveData<ArrayList<SearchUser>>()
 
     fun setUserList(username: String) {
@@ -50,5 +58,53 @@ class SearchViewModel : ViewModel() {
 
     fun getUserList(): LiveData<ArrayList<SearchUser>> {
         return listUsersLiveData
+    }
+
+    fun openDetailUser(username: String?, context: Context) {
+        val client = GithubAccess.createClient()
+        client.get(username?.let { GithubAccess.urlDetailUser(it) }, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?
+            ) {
+                try {
+                    val result = JSONObject(responseBody?.let { String(it) })
+                    val name = result.getString("name")
+                    val company = result.getString("company")
+                    val location = result.getString("location")
+                    val followers = result.getInt("followers").toString()
+                    val following = result.getInt("following").toString()
+                    val repository = result.getInt("public_repos").toString()
+                    val avatarUrl = result.getString("avatar_url")
+                    val htmlUrl = result.getString("html_url")
+                    val githubUser = User(
+                        username,
+                        name,
+                        location,
+                        repository,
+                        company,
+                        followers,
+                        following,
+                        avatarUrl,
+                        htmlUrl
+                    )
+                    val mIntent = Intent(context, ProfileActivity::class.java)
+                    mIntent.putExtra(ProfileActivity.EXTRA_USER, githubUser)
+                    context.startActivity(mIntent)
+                } catch (e: Exception) {
+                    Log.d(TAG, "onFailedToFetch: ${e.message}")
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?,
+                error: Throwable?
+            ) {
+                Log.d(TAG, "onFailure: ${error?.message}")
+            }
+        })
     }
 }
